2c662f1d27f192e37be1450ef119f12962767585
1300173 <136040432+1300173@users.noreply.github.com>
2023-06-09 05:59:40 +0000

    initial revision

---
+++ DiceGame.java
@@ -0,0 +1,11 @@
+public class DiceGame{
+    public static void main (String[] args)throws Exception {
+        System.out.println("Rolling dice...");
+        int rand1 = (int)(Math.random() * 6) + 1;
+        int rand2 = (int)(Math.random() * 6) + 1;
+        int sum = rand1 + rand2;
+        System.out.println("Die 1:" + rand1);
+        System.out.println("Die 2:" + rand2);
+        System.out.println("Total value: " + sum);
+    }
+}
