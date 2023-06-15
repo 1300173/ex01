0137ac399ad8321d6189367d5af3e1e48e0e6937
1300173 <136040432+1300173@users.noreply.github.com>
2023-06-15 03:03:57 +0000

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 public class DiceGame{
     public static void main (String[] args)throws Exception {
+        System.out.println("What is your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello, "+ name+"!");
         System.out.println("Rolling dice...");
         int rand1 = (int)(Math.random() * 6) + 1;
         int rand2 = (int)(Math.random() * 6) + 1;
         int sum = rand1 + rand2;
         System.out.println("Die 1:" + rand1);
         System.out.println("Die 2:" + rand2);
-        System.out.println("Total value: " + sum);
+        System.out.println("Total value: "+ sum );
     }
 }
